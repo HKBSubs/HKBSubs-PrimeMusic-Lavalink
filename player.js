b34a5a5bf4447b401076c895592a525047a37201
Kushal Mitra <84443457+HKBSubs@users.noreply.github.com>
2024-06-21 13:01:35 +0530

    Update player.js
    Lava Link Update

--- player.js
+++ player.js
@@ -5,10 +5,10 @@ const { queueNames } = require("./commands/play");
 function initializePlayer(client) {
     const nodes = [
         {
-            host: "four.lavalink.api.timelessnesses.me",
-            port: 80,
-            password: "youshallnotpass",
-            secure: false
+            host: "lava-v3.ajieblogs.eu.org",
+            port: 443,
+            password: "https://dsc.gg/ajidevserver",
+            secure: true
         },
     ];
 
